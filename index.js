5fb3b9db4111a07540406ce47221065652396cdd
Prakasa <myogaprakasa@gmail.com>
2023-09-13 15:28:46 +0700

    add new displaying list feature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+// adding new to do list
 function addNew() {
 
     if (document.getElementById("input") == null) {
@@ -7,6 +7,13 @@ function addNew() {
         let inputList = document.createElement("input")
         inputList.setAttribute("id", "input")
         document.getElementById("user-input").appendChild(inputList)
+
+        let enterButton = document.createElement("button")
+        enterButton.setAttribute("id", "enterButton")
+        enterButton.setAttribute("onclick", "enterInput()")
+        enterButton.textContent = "Enter"
+        document.getElementById("user-input").appendChild(enterButton)
+        
 
         let cancelButton = document.createElement("button")
         cancelButton.setAttribute("id", "cancelButton")
@@ -16,13 +23,30 @@ function addNew() {
          
     } 
 }
+
+//cancelling input
 function cancelInput() {
 
     let removeInput = document.getElementById("input")
     removeInput.remove()
 
+    let removeEnterButton = document.getElementById("enterButton")
+    removeEnterButton.remove()
+
     let removeCancelButton = document.getElementById("cancelButton")
     removeCancelButton.remove()
 }
-    
+
+// displaying input from user, also clears the input bar (by deleting it)
+function enterInput(){
+
+    let enter = document.getElementById("input").value
+    let unorderedList = document.createElement("p")
+    unorderedList.textContent = "- " + enter
+    document.getElementById("toDoList").appendChild(unorderedList)
+    cancelInput()
+
+}
+ 
+// next: make delete function for each of the list (idk how to do it)
     
