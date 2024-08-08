d9001f688f366bd9194d6015023c578849252b50
GustaveRyder <myogaprakasa@gmail.com>
2024-08-08 21:00:58 +0700

    simplifying

--- index.js
+++ index.js
@@ -1,63 +1,28 @@
 
 // IMPORTANT! I NEED TO THINK ABOUT THIS INTERFACE! I dont think it's user friendly
 
-// adding new to do list
-function addNew() {
 
-    if (document.getElementById("input") == null) {
 
-        let inputList = document.createElement("input")
-        inputList.setAttribute("id", "input")
-        document.getElementById("user-input").appendChild(inputList)
 
-        let enterButton = document.createElement("button")
-        enterButton.setAttribute("id", "enterButton")
-        enterButton.setAttribute("onclick", "enterInput()")
-        enterButton.textContent = "Enter"
-        document.getElementById("user-input").appendChild(enterButton)
-        
-
-        let cancelButton = document.createElement("button")
-        cancelButton.setAttribute("id", "cancelButton")
-        cancelButton.setAttribute("onclick", "cancelInput()")
-        cancelButton.textContent = "Cancel"
-        document.getElementById("user-input").appendChild(cancelButton)
-         
-    } 
-}
-
-//cancelling input
-function cancelInput() {
-
-    let removeInput = document.getElementById("input")
-    removeInput.remove()
-
-    let removeEnterButton = document.getElementById("enterButton")
-    removeEnterButton.remove()
-
-    let removeCancelButton = document.getElementById("cancelButton")
-    removeCancelButton.remove()
-}
-
-// displaying input from user, also clears the input bar (by deleting it)
+// adding new to-do list, also clears the input bar
 function enterInput(){
 
-    let enter = document.getElementById("input").value
-    let unorderedList = document.createElement("li")
-    unorderedList.textContent = enter
-    document.getElementById("toDoList").appendChild(unorderedList)
-    cancelInput()
+    let enter = document.getElementById("userInput").value
+    if(enter !== ""){
+        let unorderedList = document.createElement("li")
+        unorderedList.textContent = enter
+        document.getElementById("toDoList").appendChild(unorderedList)
+        // actually removes the list by itself, no need for fancy stuff
+        let removeList = document.createElement("button")
+        removeList.textContent = "Delete"
+        removeList.addEventListener("click", function(){
 
-    // actually removes the list by itself, no need for fancy stuff
-    let removeList = document.createElement("button")
-    removeList.textContent = "Delete"
-    removeList.addEventListener("click", function(){
+            unorderedList.remove()
 
-        unorderedList.remove()
-
-    })
-    unorderedList.appendChild(removeList)
-
+        })
+        unorderedList.appendChild(removeList)
+    }
+    
 }
  
 // next: adding check marks and display finished task
