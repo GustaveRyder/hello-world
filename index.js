ff9812a0370b44c7a90dd7f0cb0dc2d6fc669db2
prakasa <myogaprakasa@gmail.com>
2025-05-08 12:28:16 +0700

    finishing the project

--- index.js
+++ index.js
@@ -2,44 +2,73 @@
 // IMPORTANT! I NEED TO THINK ABOUT THIS INTERFACE! I dont think it's user friendly
 
 
+// next: display finished task
 
+    
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+let addByEnter = document.getElementById("userInput")
+
+addByEnter.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("enterButton").click()
+    }
+  });
 
 // adding new to-do list, also clears the input bar
 function enterInput(){
-
+    
     let enter = document.getElementById("userInput").value
     
     
     if(enter !== ""){
-        let unorderedList = document.createElement("input")
-        unorderedList.setAttribute("type","checkbox")
-        let toDoList = document.createElement("label")
-        let addBreakLine = document.createElement("br")
+        
+        let toDoList = document.createElement("li")
+        let listButton = document.createTextNode(enter)
         
         
-
-        toDoList.textContent = enter
+        
         document.getElementById('userInput').value = ""
         let attachToTheList = document.getElementById("theList")
-        attachToTheList.appendChild((unorderedList))
-        attachToTheList.appendChild((toDoList))
+        
+        
+        attachToTheList.appendChild(toDoList)
+        toDoList.appendChild(listButton)
         
         // actually removes the list by itself, no need for fancy stuff
-        let removeList = document.createElement("button")
-        removeList.textContent = "Delete"
+        let removeList = document.createElement("SPAN")
+        removeList.className = "remove"
+        let removeTxt = document.createTextNode("\u00D7")
         removeList.addEventListener("click", function(){
 
-            unorderedList.remove()
+            
             toDoList.remove()
             removeList.remove()
-            addBreakLine.remove()
+            
 
         })
-        attachToTheList.appendChild(removeList)
-        attachToTheList.appendChild(addBreakLine)
+        toDoList.appendChild(removeList)
+        removeList.appendChild(removeTxt)
+        
+    } else {
+        alert("You must write something")
     }
     
 }
  
-// next: display finished task
+
+
     
+
+
+
+
+    
+
